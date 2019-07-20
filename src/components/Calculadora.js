2cf4f6bda4b01004c0729a59fd0d074bcf169847
devsp011 <contatodevsp@gmail.com>
2019-07-20 01:59:51 -0300

    implementando o estado e as contas

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,91 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import Display from './Display'
 import Botao from './Botao'
 
 
-// const estado_padrao = {
-//     valorDisplay: '0',
-//     limparDisplay: false,
-//     operacao: null,
-//     valores: [0, 0],
-//     indice: 0
-// }
-
 export default function Calculadora() {
 
-    function _limpar() {
-
-    }
-
-    function _operacao(operacao) {
-        
-//             if (this.state.valores[0] === 0) return
-
-//             if (this.state.indice === 0) {
-
-//                 this.setState({ operacao, indice: 1, limparDisplay: true })
-
-//             } else {
-                
-//                 const igual = operacao === '='
-//                 const operacaoAntiga = this.state.operacao
-
-//                 const valores = [...this.state.valores]
-//                 try {
-//                     valores[0] = eval(`${valores[0]} ${operacaoAntiga} ${valores[1]}`) 
-//                 } catch (error) { valores[0] = this.state.valores[0] }
-
-//                 valores[1] = 0
-
-//                 this.setState({
-//                     valorDisplay: valores[0],
-//                     operacao: igual ? null : operacao,
-//                     indice: igual ? 0 : 1,
-//                     limparDisplay: !igual,
-//                     valores
-//                 })
-//             }
-    }
+    const [valorDisplay, setValorDisplay] = useState('0')
+    const [limparDisplay, setLimparDisplay] = useState(false)
+    const [operacao, setOperacao] = useState(null)
+    const [valores, setValores] = useState([0, 0])
+    const [indice, setIndice] = useState(0)
 
     function _adicionar(label) {
-        //validar: apenas um ponto no displey
-//             if (label === '.' && this.state.valorDisplay.includes('.')) return
 
-//             //validar: retirar zero a esquerda e Validar: checar variavel limparDisplay
-//             const limparDisplay = this.state.valorDisplay === '0' || this.state.limparDisplay
-//             const valorAtual = limparDisplay ? '' : this.state.valorDisplay
+        // validar: apenas um ponto no displey
+        if (label === '.' && valorDisplay.includes('.')) return
 
+        //validar: retirar zero a esquerda e Validar: checar variavel limparDisplay
+        const check_limparDisplay = valorDisplay === '0' || limparDisplay
+        const valorAtual = check_limparDisplay ? '' : valorDisplay
 
-//             const valorDisplay = valorAtual + label
+        const novo_valorDisplay = valorAtual + label
+        const novos_valores = [...valores]
 
-//             const valores = [...this.state.valores]
-//             if (label !== '.') valores[this.state.indice] = parseFloat(valorDisplay) 
+        if (label !== '.') novos_valores[indice] = parseFloat(novo_valorDisplay)
 
-//             this.setState({ valorDisplay, limparDisplay: false, valores })
+        setValorDisplay(novo_valorDisplay)
+        setLimparDisplay(false)
+        setValores(novos_valores)
+    }
+
+    function _operacao(operacao_clicada) {
+
+        if (valores[0] === 0) return
+
+        if (indice === 0) {
+
+            setIndice(1)
+            setOperacao(operacao_clicada)
+            setLimparDisplay(true)
+
+        } else {
+
+            const igual = operacao_clicada === '='
+            const novos_valores = [...valores]
+
+            try {
+                novos_valores[0] = eval(`${novos_valores[0]} ${operacao} ${novos_valores[1]}`)
+            } catch (error) { novos_valores[0] = valores[0] }
+
+            novos_valores[1] = 0
+
+            setValorDisplay(novos_valores[0])
+            setLimparDisplay(!igual)
+            setOperacao(igual ? null : operacao_clicada)
+            setValores(novos_valores)
+            setIndice(igual ? 0 : 1)
+
+        }
+    }
+
+    function _limpar() {
+        setValorDisplay('0')
+        setLimparDisplay(false)
+        setOperacao(null)
+        setValores([0, 0])
+        setIndice(0)
     }
 
     return (
         <div className='calculadora'>
-            <Display valor={'valorDisplay'}></Display>
+            <Display valor={valorDisplay}></Display>
 
-            <Botao label='AC' click={_limpar} span='span-3'></Botao>
-            <Botao label='/' click={_operacao} operacao></Botao>
-            <Botao label='7' click={_adicionar} ></Botao>
-            <Botao label='8' click={_adicionar} ></Botao>
-            <Botao label='9' click={_adicionar} ></Botao>
-            <Botao label='*' click={_operacao} operacao></Botao>
-            <Botao label='4' click={_adicionar} ></Botao>
-            <Botao label='5' click={_adicionar} ></Botao>
-            <Botao label='6' click={_adicionar} ></Botao>
-            <Botao label='-' click={_operacao} operacao></Botao>
-            <Botao label='1' click={_adicionar} ></Botao>
-            <Botao label='2' click={_adicionar} ></Botao>
-            <Botao label='3' click={_adicionar} ></Botao>
-            <Botao label='+' click={_operacao} operacao></Botao>
-            <Botao label='0' click={_adicionar} span='span-2'></Botao>
-            <Botao label='.' click={_adicionar} ></Botao>
-            <Botao label='=' click={_operacao} operacao></Botao>
+            <Botao label='AC' funcao={_limpar} span='span-3'></Botao>
+            <Botao label='/' funcao={_operacao} operacao></Botao>
+            <Botao label='7' funcao={_adicionar} ></Botao>
+            <Botao label='8' funcao={_adicionar} ></Botao>
+            <Botao label='9' funcao={_adicionar} ></Botao>
+            <Botao label='*' funcao={_operacao} operacao></Botao>
+            <Botao label='4' funcao={_adicionar} ></Botao>
+            <Botao label='5' funcao={_adicionar} ></Botao>
+            <Botao label='6' funcao={_adicionar} ></Botao>
+            <Botao label='-' funcao={_operacao} operacao></Botao>
+            <Botao label='1' funcao={_adicionar} ></Botao>
+            <Botao label='2' funcao={_adicionar} ></Botao>
+            <Botao label='3' funcao={_adicionar} ></Botao>
+            <Botao label='+' funcao={_operacao} operacao></Botao>
+            <Botao label='0' funcao={_adicionar} span='span-2'></Botao>
+            <Botao label='.' funcao={_adicionar} ></Botao>
+            <Botao label='=' funcao={_operacao} operacao></Botao>
         </div>
     )
 }
