13de0007cc16748569f282d2a10a95f006a2bf3e
DevSp <contatodevsp@gmail.com>
2020-05-13 19:28:28 -0300

    Updates

---
+++ precache-manifest.20e4894bcdabf324f20a65ed3d044740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b21315876b12435a3198314c984cfc",
+    "url": "/calculadora-react/index.html"
+  },
+  {
+    "revision": "51e2734c00fc5f011be4",
+    "url": "/calculadora-react/static/css/main.58d88c49.chunk.css"
+  },
+  {
+    "revision": "2e9cb7034534980d7d9d",
+    "url": "/calculadora-react/static/js/2.02054e0e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/calculadora-react/static/js/2.02054e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e2734c00fc5f011be4",
+    "url": "/calculadora-react/static/js/main.22fc5354.chunk.js"
+  },
+  {
+    "revision": "8c51c120b0ca110f6811",
+    "url": "/calculadora-react/static/js/runtime-main.00666d3c.js"
+  }
+]);
